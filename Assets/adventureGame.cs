0ca0f03f5b3b249987189ec0e143e4734bdb7c06
LAPTOP-HT0THONL\Ajay <ajayjyothish007@gmail.com>
2018-08-05 03:56:02 +0530

    Initial commit

---
+++ Assets/adventureGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class adventureGame : MonoBehaviour {
+
+    [SerializeField] Text textComponent;
+    [SerializeField] state startingState;
+
+    state state ;
+   
+	// Use this for initialization
+	void Start () {
+        state = startingState;
+        textComponent.text = state.getStateStory();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        manageState();
+	}
+
+    private void manageState()
+    {
+        var nextState = state.getNextStates();
+        for (int permit = 0; permit < nextState.Length; permit++)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + permit))
+            {
+               
+                state = nextState[permit];
+            }
+            textComponent.text = state.getStateStory();
+
+        }
+        
+
+
+
+    }
+}
---
+++ Assets/state.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "states")]
+public class state : ScriptableObject {
+
+    [TextArea(10, 14)] [SerializeField] string storyText;
+    [SerializeField] state[] nextStates;
+
+    public string getStateStory()
+    {
+        return storyText;
+    }
+
+    public state[] getNextStates()
+    {
+        return nextStates;
+    }
+}   
